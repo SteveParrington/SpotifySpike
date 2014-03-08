fadb75abdf03ab6b0c8bfa6df16df9454c54e635
Steve Parrington <steveparrington@gmail.com>
2014-03-08 16:57:58 +0000

    Updated ControllerThread with basic command execution logic and added requirements.txt

--- backend_service.py
+++ backend_service.py
@@ -1,4 +1,3 @@
-import zmq
 import threading
 import queue
 import spotify
@@ -6,14 +5,29 @@ import spotify
 class ControllerThread(threading.Thread):
 
     def __init__(self, out_queue):
-        router = {
+        self.router = {
             'login': self.login,
             'logout': self.logout,
-            'album': self.get_album
+            'album': self.get_album,
+            'exit': self.exit
         }
         self.in_queue = queue.Queue()
         self.out_queue = out_queue
         self.session = spotify.Session()
+        self.active = True
+
+    def run(self):
+        while self.active:
+            self.execute_command()
+                
+    def execute_command(self):
+        try:
+            command = self.in_queue.get_nowait()
+            opcode = command.opcode
+            args = command.args
+            self.router[opcode](*args)
+        except queue.Empty:
+            pass
 
     def login(self, username, password): 
         pass
@@ -23,3 +37,6 @@ class ControllerThread(threading.Thread):
 
     def get_album(self, album_id):
         pass
+
+    def exit(self):
+        self.active = False
