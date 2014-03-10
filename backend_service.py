bc12db48c5a89a2d6b55ac81e308d5b1ffcd95c2
Steve Parrington <steveparrington@gmail.com>
2014-03-10 22:12:29 +0000

    Pause functionality now available as well as some general clean up around unloading tracks from the player

--- backend_service.py
+++ backend_service.py
@@ -3,22 +3,84 @@ import Queue
 import spotify
 import alsaaudio
 
-class BufferThread(threading.Thread):
+class AudioError(Exception):
+    pass
+
+
+class AudioQueue(Queue.Queue):
+    ''' Made to enable a queue used for audio to be cleared when necessary, it also
+        disables threads from trying to put new data into the queue while it is 
+        being cleared '''
 
     def __init__(self):
-        self.in_queue = Queue.Queue()
+        self.allow_put = True
+        # Queue is an old style object so we need to do old style inheritance
+        # Boo Python 2!
+        Queue.Queue.__init__(self)
+
+    def put(self, *args):
+        if self.allow_put:
+            Queue.Queue.put(self, *args)
+        else:
+            raise AudioError('CustomQueue.put was attempted while the queue was clearing')
+
+    def clear(self):
+        self.allow_put = False
+        try:
+            while True:
+                self.get_nowait()
+        except Queue.Empty:
+            self.allow_put = True
+
+
+class AudioThread(threading.Thread):
+
+    def __init__(self):
+        self.audio_queue = AudioQueue()
+        self.command_queue = Queue.Queue()
+        self.play_audio = True
         self.pcm = alsaaudio.PCM(type=alsaaudio.PCM_PLAYBACK, mode=alsaaudio.PCM_NORMAL)
         self.pcm.setchannels(2)
         self.pcm.setrate(44100)
         self.pcm.setformat(alsaaudio.PCM_FORMAT_S16_LE)
         self.pcm.setperiodsize(2048)
         self.active = True
-        super(BufferThread, self).__init__()
+        super(AudioThread, self).__init__()
 
     def run(self):
         while self.active:
-            data = self.in_queue.get() 
-            self.pcm.write(data)
+            self.process_audio()
+            self.execute_command()
+
+    def process_audio(self):
+        try:
+            if self.play_audio:
+                data = self.audio_queue.get_nowait()
+                self.pcm.write(data)
+        except Queue.Empty:
+            pass
+
+    def execute_command(self):
+        valid_opcodes = ('play', 'pause', 'clear_audio', 'exit')
+        try:
+            command = self.command_queue.get_nowait()
+            if command.opcode in valid_opcodes:
+                getattr(self, command.opcode)()
+        except Queue.Empty:
+            pass
+
+    def play(self):
+        self.play_audio = True
+
+    def pause(self):
+        self.play_audio = False
+
+    def clear_audio(self):
+        self.audio_queue.clear()
+
+    def exit(self):
+        self.active = False
+
 
 class ControllerThread(threading.Thread):
 
@@ -28,9 +90,11 @@ class ControllerThread(threading.Thread):
         self.session = spotify.Session()
         self.active = True
         self.register_callbacks()
-        self.buffer_thread = BufferThread()
-        self.buffer_queue = self.buffer_thread.in_queue
-        self.buffer_thread.start()
+        self.audio_thread = AudioThread()
+        self.audio_queue = self.audio_thread.audio_queue
+        self.audio_command_queue = self.audio_thread.command_queue
+        self.audio_thread.start()
+        self.next_song = None
         self.current_song = None
         super(ControllerThread, self).__init__()
 
@@ -41,11 +105,13 @@ class ControllerThread(threading.Thread):
 
     def execute_command(self):
         valid_opcodes = ('login', 'logout', 'get_album', 'get_song', 'exit',
-                         'play')
+                         'play', 'pause')
         try:
             command = self.in_queue.get_nowait()
             if command.opcode in valid_opcodes:
                 getattr(self, command.opcode)(*command.args)
+            else:
+                self.out_queue.put('Invalid command, try again')
         except Queue.Empty:
             pass
 
@@ -64,20 +130,36 @@ class ControllerThread(threading.Thread):
     def get_song(self, song_id):
         song = spotify.Track('spotify:track:{0}'.format(song_id))
         song.load()
-        self.current_song = song
+        self.next_song = song
         response = "Title: {0}\nArtist: {1}".format(song.name, song.album.artist.name)
         self.out_queue.put(response)
 
     def exit(self):
         self.active = False
+        self.audio_command_queue.put(Command('exit'))
 
     def play(self):
-        if self.current_song is not None:
+        if self.next_song is not None and self.current_song is None:
             player = self.session.player
+            self.current_song = self.next_song
+            self.next_song = None
             player.load(self.current_song)
             player.play()
             self.out_queue.put('Playing {0}'.format(self.current_song.name))
-        self.out_queue.put('No song loaded')
+        elif self.current_song is not None:
+            self.session.player.play()
+            self.audio_command_queue.put(Command('play'))
+            self.out_queue.put('Playing {0}'.format(self.current_song.name))
+        else:
+            self.out_queue.put('No song loaded')
+
+    def pause(self):
+        if self.current_song is not None:
+            self.session.player.play(False)
+            self.audio_command_queue.put(Command('pause'))
+            self.out_queue.put('Paused {0}'.format(self.current_song.name))
+        else:
+            self.out_queue.put('Not currently playing anything!')
 
     def login_complete(self, session, error_type):
         self.out_queue.put_nowait("Login successful!")
@@ -86,14 +168,19 @@ class ControllerThread(threading.Thread):
         self.out_queue.put_nowait("Logged out")
 
     def music_delivery(self, session, audio_format, frames, num_frames):
-        self.buffer_queue.put(frames)
+        self.audio_queue.put(frames)
         return num_frames
+
+    def end_of_track(self, session):
+        self.session.player.unload()
+        self.current_song = None
 
     def register_callbacks(self):
         callbacks = {
             'LOGGED_IN': self.login_complete,
             'LOGGED_OUT': self.logout_complete,
-            'MUSIC_DELIVERY': self.music_delivery
+            'MUSIC_DELIVERY': self.music_delivery,
+            'END_OF_TRACK': self.end_of_track
         }
         for name in callbacks:
             self.session.on(getattr(spotify.SessionEvent, name), callbacks[name])
